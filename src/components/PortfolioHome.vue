8c34e229e4a995574bf4139bcf504e780835d84a
unknown <meyer.744@gmail.com>
2021-05-10 21:57:22 -0400

    added refine products project

--- src/components/PortfolioHome.vue
+++ src/components/PortfolioHome.vue
@@ -11,7 +11,7 @@
       <div class="home-right">
         <ul class="profile-blurb">
           <h2 class="profile-pic-heading">Jack Meyer</h2>
-          <li class="profile-blurbs">Front-End Web Developer</li>
+          <li class="profile-blurbs">Frontend Developer</li>
           <li class="profile-blurbs">Graphic Designer</li>
           <li class="profile-blurbs">Digital Marketer</li>
           <li class="profile-blurbs">Proud father of two awesome kids</li>
